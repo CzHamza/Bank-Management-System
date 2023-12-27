aef2ab7415f40de9e490979f7cc0ce529e18f0cf
CzHamza <hamza.cengiz@ozu.edu.tr>
2023-12-27 15:55:05 +0300

    Getters and setters for Account

--- src/domain/Account.java
+++ src/domain/Account.java
@@ -9,4 +9,46 @@ public class Account {
     private double balance;
     private User owner;
     private List<Transaction> transactions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+
 }
