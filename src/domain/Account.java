a2767872d0c9ba69cfeb0bf8dd83f2dfbcbd45ac
CzHamza <hamza.cengiz@ozu.edu.tr>
2023-12-27 16:17:36 +0300

    Added methods to the Account

--- src/domain/Account.java
+++ src/domain/Account.java
@@ -1,6 +1,8 @@
 package domain;
 
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public class Account {
@@ -49,6 +51,73 @@ public class Account {
     public void setTransactions(List<Transaction> transactions) {
         this.transactions = transactions;
     }
+
+    public void deposit(double amount, Account sender) {
+        if (amount > 0) {
+            this.balance += amount;
+
+            Transaction depositTransaction = new Transaction(
+                    LocalDate.now(),
+                    amount,
+                    "Deposit",
+                    sender,
+                    this,
+                    "Deposit"
+            );
+
+            this.transactions.add(depositTransaction);
+
+            System.out.println("Deposit of " + amount + " made successfully.");
+        } else {
+            System.out.println("Invalid deposit amount. Please provide a positive value.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        Date date = new Date();
+        if (amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+
+            Transaction withdrawTransaction = new Transaction(
+                    LocalDate.now(),
+                    amount,
+                    "Withdrawal amount: " + amount,
+                    this,
+                    null,
+                    "Withdrawal"
+            );
+
+            this.transactions.add(withdrawTransaction);
+
+            System.out.println("Withdrawal of " + amount + " made successfully.");
+            return true;
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+            return false;
+        }
+    }
+
+    public boolean transfer(double amount, Account receiver, LocalDate date) {
+        if (withdraw(amount)) {
+            receiver.deposit(amount, this);
+            Transaction transferTransaction = new Transaction(
+                    date,
+                    amount,
+                    "Transfer to " + receiver.getOwner().getName(),
+                    this,
+                    receiver,
+                    "Transfer"
+            );
+            this.transactions.add(transferTransaction);
+            System.out.println("Transfer of " + amount + " to account " + receiver.getId() + " made successfully.");
+            return true;
+        } else {
+            System.out.println("Transfer failed: Insufficient balance or invalid amount.");
+            return false;
+        }
+    }
+
+
 
 
 }
