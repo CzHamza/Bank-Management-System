278a855208dea8a321c796d5eaa57a9a74e9185d
nisan-tascioglu-ozu <100801046+nisantascioglu@users.noreply.github.com>
2024-01-03 13:32:14 +0300

    Update TransactionProcessor.java

--- src/Services/TransactionProcessor.java
+++ src/Services/TransactionProcessor.java
@@ -1,9 +1,43 @@
 package Services;
 
+import domain.Account;
 import domain.Transaction;
 
 public class TransactionProcessor {
-    public boolean processTransaction(Transaction transaction) {
-        return transaction.makeTransaction();
+
+
+    public static boolean validateTransaction(Transaction transaction) {
+        return transaction != null && transaction.validate();
+    }
+
+  
+    public static void updateAccountBalance(Transaction transaction) {
+        if (transaction != null && transaction.validate()) {
+            Account senderAccount = transaction.getSenderAccount();
+            Account receiverAccount = transaction.getReceiverAccount();
+            double amount = transaction.getAmount();
+
+            if (senderAccount != null && receiverAccount != null && amount > 0) {
+                senderAccount.withdraw(amount);
+                receiverAccount.deposit(amount, senderAccount);
+            }
+        }
+    }
+
+    
+    public static boolean initiateRefund(Transaction transaction) {
+        if (transaction != null && transaction.validate() && "Withdrawal".equals(transaction.getType())) {
+            Account senderAccount = transaction.getSenderAccount();
+            Account receiverAccount = transaction.getReceiverAccount();
+            double amount = transaction.getAmount();
+
+            if (senderAccount != null && receiverAccount != null && amount > 0) {
+                if (senderAccount.deposit(amount, receiverAccount) && receiverAccount.withdraw(amount)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
+
