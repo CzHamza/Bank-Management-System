c63dc5622bfc5f48df914f7f4eb0b616e7656c11
CzHamza <hamza.cengiz@ozu.edu.tr>
2024-01-03 13:45:35 +0300

    app

--- src/App.java
+++ src/App.java
@@ -1,6 +1,31 @@
+import Services.Dashboard;
+import Services.Login;
+import Services.UserProfile;
+import domain.Fund;
+import domain.Transaction;
+import domain.User;
+
 public class App {
 
     public static void main(String[] args) {
+        User user = new User("username", "password");
+        Login login = new Login(user);
+        Dashboard dashboard = new Dashboard();
+        Fund funds = new Fund();
+        Transaction transaction = new Transaction();
+        UserProfile userProfile = new UserProfile();
 
+        if (login.validateUser(user.getPassword())) {
+            dashboard.displaySummary();
+            dashboard.showNotifications();
+            dashboard.showHelp();
+
+            funds.displayFunds();
+            transaction.showHistory();
+            userProfile.showProfile();
+        } else {
+            System.out.println("Login failed. Please check your username and password.");
+        }
     }
+
 }
