9bda75138619104410466fba2725b7a864eeaaa0
nisan-tascioglu-ozu <100801046+nisantascioglu@users.noreply.github.com>
2024-01-03 14:47:34 +0300

    Update TransactionLogger.java

--- src/Services/TransactionLogger.java
+++ src/Services/TransactionLogger.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class TransactionLogger {
     private List<Transaction> transactionLogs;
 
-    // Constructor
+   
     public TransactionLogger() {
         this.transactionLogs = new ArrayList<>();
     }
 
-    // Method to log a transaction
+   
     public void logTransaction(Transaction transaction) {
         transactionLogs.add(transaction);
     }
 
-    // Method to retrieve all transaction logs
+    
     public List<Transaction> retrieveLogs() {
         return new ArrayList<>(transactionLogs);
     }
