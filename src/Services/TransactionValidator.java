02119590c3b1ada80b6591fa5f700641123b9a8c
nisan-tascioglu-ozu <100801046+nisantascioglu@users.noreply.github.com>
2024-01-03 13:37:00 +0300

    Update TransactionValidator.java

--- src/Services/TransactionValidator.java
+++ src/Services/TransactionValidator.java
@@ -3,7 +3,15 @@ package Services;
 import domain.Transaction;
 
 public class TransactionValidator {
-    public boolean validateTransaction(Transaction transaction){
-        return transaction.validate();
+
+    public boolean validate(Transaction transaction) {
+        return transaction != null && transaction.validate();
+    }
+
+    public boolean checkSufficientBalance(Transaction transaction) {
+        if (transaction != null && transaction.getSenderAccount() != null) {
+            return transaction.getAmount() <= transaction.getSenderAccount().getBalance();
+        }
+        return false;
     }
 }
